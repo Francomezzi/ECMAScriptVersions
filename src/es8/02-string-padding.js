c4aa2d598b9c9a927456bd82f260c3d9df621648
Franco Mezzi <mezzifrancodev@gmail.com>
2023-10-25 15:46:41 -0300

    String padding, trailing commas y async Functions

---
+++ src/es8/02-string-padding.js
@@ -0,0 +1,7 @@
+const string = "Hola ";
+
+console.log(string.padEnd(11,"Franco"));
+
+const string1 = 'Asheee';
+
+console.log(string1.padStart(10,"_"));
---
+++ src/es8/03-trailing-commas.js
@@ -0,0 +1,4 @@
+const nums = [2,3,1,24,14,46,76,1,3,43, , , ,23];
+
+console.log(nums);
+console.log(nums.length);
