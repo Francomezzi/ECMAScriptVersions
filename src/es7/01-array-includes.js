fc98a734e782e97d741de36bdc4e613370c2b1fb
Franco Mezzi <mezzifrancodev@gmail.com>
2023-10-25 11:43:51 -0300

    Agregamos carpeta es7 y exponecials, arrays includes

---
+++ src/es7/01-array-includes.js
@@ -0,0 +1,8 @@
+let array = [2,3,4,5,6,7,8,8,9,12,3421,231,543,43];
+
+let trueor = array.includes(1);
+console.log(trueor);
+
+const list = ["Oscar","Franco"];
+
+console.log(list.includes("Oscar"));
