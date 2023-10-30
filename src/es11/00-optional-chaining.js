ca9f4f0976379649a17d455dd6c11693bc11b4c8
Franco Mezzi <mezzifrancodev@gmail.com>
2023-10-30 20:12:35 -0300

    ES11 optional chaining, bigInt,Nullish,promise allSettled,global This, matchAll y dynamic import

---
+++ src/es11/00-optional-chaining.js
@@ -0,0 +1,10 @@
+const users ={
+    gndx:{
+        country: "Arg"
+    },
+    ana:{
+        country: "CO"
+    }
+}
+
+console.log(users?.bebe?.country);
---
+++ src/es11/01-bigInt.js
@@ -0,0 +1,4 @@
+const aBigNumber = 321312471247127847812784n;
+const anotherBigN = BigInt(321312471247127847812784);
+console.log(aBigNumber);
+console.log(anotherBigN);
